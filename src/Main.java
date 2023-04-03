7d16d9ab77ed8b6d2bfa7f134f5ee7b32b476e60
HwangJunha <tarot1415@gmail.com>
2023-04-04 00:15:35 +0900

    [P1427] 선택 정렬

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = sc.nextInt();
+        String str = sc.next();
+        int[] arr = new int[str.length()];
+        for(int i=0; i<str.length(); i++){
+            arr[i] = Integer.parseInt(str.substring(i, i+1));
         }
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length-1; j++){
-               int temp = arr[j];
-               if(arr[j+1] < arr[j]){
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
-               }
+
+        for(int i=0; i<str.length(); i++){
+            int max = i;
+            for(int j=i+1; j<str.length(); j++){
+                if(arr[j]>arr[max]){// 내림차순으로 최댓값 찾음
+                    max = j;
+                }
+
+            }
+            if(arr[i] < arr[max]){
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
             }
         }
-        for(int i=0; i<n; i++){
-            System.out.println(arr[i]+" ");
+        for(int i=0; i<str.length(); i++){
+            System.out.print(arr[i]);
         }
 
     }
